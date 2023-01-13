11fd0c175b45bbb44bcae9903c12ba910aafc9ba
Ikbal Gundogdu <ikbalgun98@gmail.com>
2023-01-13 09:30:24 +0100

    u9

--- 9/php/db_connection.php
+++ 9/php/db_connection.php
@@ -5,37 +5,3 @@ $dbpass = "123";
 $dbname = "webb2";
 
 $dbc = new mysqli("localhost", $dbuser, $dbpass, $dbname);
-
-// $c1 = "CREATE TABLE `webb2`.`products` (
-//     `product_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT , 
-//     `name` VARCHAR(255) NOT NULL , 
-//     `price` FLOAT NOT NULL , 
-//     `img_src` VARCHAR(255) NOT NULL , 
-//     PRIMARY KEY (`product_id`),
-//     ) ENGINE = InnoDB;";
-
-// $c2 = "CREATE TABLE `webb2`.`cart_items` (
-//     `item_id` INT(10) NOT NULL AUTO_INCREMENT , 
-//     `session_id` INT(10) NOT NULL , 
-//     `product_id` INT(10) NOT NULL , 
-//     `quantity` INT(10) NOT NULL , 
-//     PRIMARY KEY (`item_id`),
-//     FOREIGN KEY (`product_id`) REFERENCES `products`(`product_id`),
-//     FOREIGN KEY (`session_id`) REFERENCES `sessions`(`session_id`),
-//     ) ENGINE = InnoDB;";
-
-// $c3 = "CREATE TABLE `webb2`.`sessions` (
-//     `session_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT , 
-//     `user_id` INT(10) NOT NULL , 
-//     PRIMARY KEY (`session_id`),
-//     FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`),
-//     ) ENGINE = InnoDB;";
-
-// $c4 = "CREATE TABLE `webb2`.`users` (
-//     `user_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT , 
-//     `name` VARCHAR(255) NOT NULL , 
-//     `email` VARCHAR(255) NOT NULL , 
-//     `password` VARCHAR(255) NOT NULL , 
-//     PRIMARY KEY (`user_id`), 
-//     UNIQUE (`email`),
-//     ) ENGINE = InnoDB;";
---
+++ webb2.sql
@@ -0,0 +1,52 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+CREATE DATABASE IF NOT EXISTS `webb2` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `webb2`;
+
+CREATE TABLE `cart_items` (
+  `shopping_id` INT(10) NOT NULL AUTO_INCREMENT, 
+  `session_id` INT(10) NOT NULL, 
+  `product_id` INT(10) NOT NULL, 
+  `quantity` INT(10) NOT NULL, 
+  `item_total` int(10) UNSIGNED NOT NULL
+  PRIMARY KEY (`shopping_id`),
+  FOREIGN KEY (`product_id`) REFERENCES `products`(`product_id`),
+  FOREIGN KEY (`session_id`) REFERENCES `sessions`(`session_id`),
+) ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+CREATE TABLE `products` (
+  `product_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, 
+  `name` VARCHAR(255) NOT NULL, 
+  `price` FLOAT NOT NULL, 
+  `img_src` VARCHAR(255) NOT NULL, 
+  PRIMARY KEY (`product_id`),
+) ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+CREATE TABLE `sessions` (
+  `session_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, 
+  `user_id` int(10) DEFAULT NULL,   
+  PRIMARY KEY (`session_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`),
+) ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+CREATE TABLE `users` (
+  `user_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, 
+  `name` VARCHAR(255) NOT NULL, 
+  `email` VARCHAR(255) NOT NULL, 
+  `password` VARCHAR(255) NOT NULL, 
+  PRIMARY KEY (`user_id`), 
+  UNIQUE (`email`),
+) ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
